613a4f90ab528ab5e4eed0035a427ea1d52ab970
sohaib shoukat <sohaibshoukat94@gmail.com>
2024-02-08 01:12:40 +0500

    push

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -12,6 +12,8 @@ import { sygnet } from 'src/assets/brand/sygnet'
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
 
+// import "./style.css"
+
 // sidebar nav config
 import navigation from '../_nav'
 
@@ -24,33 +26,42 @@ const AppSidebar = () => {
   const sidebarShow = useSelector((state) => state.sidebarShow)
 
   return (
-    <CSidebar
-      className='bggray'
-      position="fixed"
-      unfoldable={unfoldable}
-      visible={sidebarShow}
-      onVisibleChange={(visible) => {
-        dispatch({ type: 'set', sidebarShow: visible })
-      }}
-    >
-      <CSidebarBrand className="d-none d-md-flex bgdark" to="/" >
-        {unfoldable ?
-          <img src={logo2} style={{ height: "50px",width:"50px" }} alt="" />
-          :
-          <img src={logo} style={{ height: "100px" }} alt="" />
+    <>
+      <CSidebar
+        className='bggray'
+        position="fixed"
+        unfoldable={unfoldable}
+        visible={sidebarShow}
+        onVisibleChange={(visible) => {
+          dispatch({ type: 'set', sidebarShow: visible })
+        }}
+      >
+        <CSidebarBrand className="d-none d-md-flex bgdark" to="/" >
+          {unfoldable ?
+            <img src={logo2} style={{ height: "50px", width: "50px" }} alt="" />
+            :
+            <img src={logo} style={{ height: "100px" }} alt="" />
 
-        }
-      </CSidebarBrand>
-      <CSidebarNav className='bgdark'>
-        <SimpleBar>
-          <AppSidebarNav items={navigation} />
-        </SimpleBar>
-      </CSidebarNav>
-      <CSidebarToggler
-        className="d-none d-lg-flex bgwhite pos"
-        onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
-      />
-    </CSidebar>
+          }
+        </CSidebarBrand>
+        <CSidebarNav className='bgdark'>
+          <SimpleBar>
+            <AppSidebarNav items={navigation} />
+          </SimpleBar>
+        </CSidebarNav>
+        {/* <CSidebarToggler
+          className="d-none d-lg-flex bgwhite pos"
+          onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
+        /> */}
+      </CSidebar>
+      
+      {/* <div className={`${unfoldable?"bgclose":"bgsection"}`}
+        onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}>
+        <div className={`${unfoldable?"closeImage":"imagesection"}`}>
+        
+        </div>
+      </div> */}
+    </>
   )
 }
 
--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import "./style.css"
 
 const DefaultLayout = () => {
+  const dispatch = useDispatch()
+  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
   const navigate = useNavigate()
   const [logincheck, setlogincheck] = useState(false)
   useEffect(() => {
@@ -10,7 +14,7 @@ const DefaultLayout = () => {
       navigate('/login')
       setlogincheck(false)
     }
-    else{
+    else {
       setlogincheck(true);
     }
 
@@ -21,8 +25,15 @@ const DefaultLayout = () => {
       {logincheck && <div>
         <AppSidebar />
         <div className="wrapper d-flex flex-column min-vh-100 bg-light">
+
           <AppHeader />
-          <div className="body flex-grow-1 px-3 bgblue py-5">
+          <div className="body flex-grow-1 px-3 bgblue py-5 relative">
+            <div className={`${unfoldable ? "bgclose" : "bgsection"}`}
+              onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}>
+              <div className={`${unfoldable ? "closeImage" : "imagesection"}`}>
+
+              </div>
+            </div>
             <AppContent />
           </div>
           {/* <AppFooter /> */}
