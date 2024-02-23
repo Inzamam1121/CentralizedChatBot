978cac7d7577ab79c85c22071deed4d2ba97174e
sohaib shoukat <sohaibshoukat94@gmail.com>
2024-02-24 03:03:54 +0500

    push

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -21,6 +21,8 @@ import BgVideo from '../../../assets/video/Intro.gif'
 import facebook from '../../../assets/images/facebook.png'
 import google from '../../../assets/images/search.png'
 
+import Logo from "../../../assets/images/Logo.png"
+
 const Login = () => {
 
   const AletContext = useContext(AlertContext);
@@ -99,26 +101,58 @@ const Login = () => {
         backgroundPosition: "center",
         backgroundAttachment: "fixed"
       }}>
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md={4} sm={8}>
-            <CCardGroup>
-              <CCard className="p-4 bgclear">
-                <CCardBody>
-                  <CForm className='text-center'>
+      <CContainer className='heightcontainer'>
+        <CRow className="justify-content-center h-100">
+          <CCol md={9} sm={8}>
+            <CCardGroup className='h-100'>
+              <CCard 
+                className="bgclear"
+                style={{
+                  borderRadius:"25px"
+                }}
+              >
+                <CCardBody className='p-0'>
+                  <div className='d-flex h-100 gap-4 align-items-center'
+                  style={{
+                    borderRadius:"100px"
+                  }}
+                  >
+                    <div 
+                      className='position-relative h-100 col-md-5 d-flex flex-column justify-content-center align-items-center'
+                      style={{
+                        backgroundColor:"rgb(0, 0, 0, 0.4)",
+                        borderTopLeftRadius:"25px",
+                        borderBottomLeftRadius:"25px",
+                        overflow:"hidden"
+                      }}
+                    >
+                      <div style={{
+                        width:"250px",
+                        height:"250px",
+                        position:"absolute",
+                        top:"-15%",
+                        left:"-20%",
+                        backgroundColor:"#000000",
+                        borderRadius:"50%",
+                        zIndex:"0"
+                      }}></div>
+                                            <div style={{
+                        width:"250px",
+                        height:"250px",
+                        position:"absolute",
+                        bottom:"-15%",
+                        right:"-20%",
+                        backgroundColor:"#000000",
+                        borderRadius:"50%",
+                        zIndex:"0"
+                      }}></div>
+                      <img src={Logo} alt="" />
+                      <h2 className='text-white fs-2 fw-bold'>Cosmic Nucleus</h2>
+                    </div>  
+                  <CForm className='text-left p-4 col-md-6'>
                     <h1 className='clwhite2'>Login</h1>
                     <p className="text-medium-emphasis clwhite2">Sign In to your account</p>
-                    <div className='borderdata'>
-                      <CAvatar size="sm" src={google} />
-                      <h2>Continue With Google</h2>
-                      <div></div>
-                    </div>
-                    <div className='borderdata'>
-                      <CAvatar size="sm" src={facebook} />
-                      <h2>Continue With Facebook</h2>
-                      <div></div>
-                    </div>
-                    <p className="text-medium-emphasis clwhite2">Or</p>
+                    
                     <CInputGroup className="mb-3">
                       <CInputGroupText className='bggray'>
                         <CIcon icon={cilUser} className='clwhite' />
@@ -146,12 +180,27 @@ const Login = () => {
                     </CInputGroup>
                     <CRow>
                       <CCol>
-                        <CButton color="primary" className="px-4 clblack bggray my-4 bgbuttonblue w-100" onClick={handleLogin} disabled={isSubmitting}>
+                        <CButton color="primary" className="px-4 clblack bggray my-1 bgbuttonblue w-100" onClick={handleLogin} disabled={isSubmitting}>
                           {isSubmitting ? "Logining" : "Login"}
                         </CButton>
                       </CCol>
                     </CRow>
+                    <p className="text-medium-emphasis clwhite2 text-center">Or</p>
+                    <div className='d-flex flex-row gap-4 justify-content-center align-items-center'>
+                      <CAvatar size="md" src={google} />
+                      <CAvatar size="md" src={facebook} />
+
+                    </div>
+                    {/* <div className='borderdata'>
+                      <h2>Continue With Google</h2>
+                      <div></div>
+                    </div>
+                    <div className='borderdata'>
+                      <h2>Continue With Facebook</h2>
+                      <div></div>
+                    </div> */}
                   </CForm>
+                  </div>
                 </CCardBody>
               </CCard>
             </CCardGroup>
