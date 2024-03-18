a9ff57e9789f1624f0ab0ed977e12838f9d7aea2
sohaib shoukat <sohaibshoukat94@gmail.com>
2024-03-18 23:21:57 +0500

    push

--- src/views/Dashboard/Settings.js
+++ src/views/Dashboard/Settings.js
@@ -58,7 +58,8 @@ const Settings = () => {
                 const data = await response.json();
                 showAlert(data.message, 'success');
             } else {
-                showAlert('Failed to Add User', 'danger');
+                const data = await response.json();
+                showAlert(data.detail, 'danger');
             }
         } catch (error) {
             showAlert('Failed to Add User', 'danger');
